df3b95aa468c8604d7dd948d3e433c47158ffd5a
raamy dib <you@example.com>
2025-05-18 16:43:04 +0200

    a

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -15,7 +15,7 @@
 void	reverse_rotate(t_list **list)
 {
 	t_list	*head;
-	t_list	*new_head;
+	t_list	*new_tail;
 	t_list	*last;
 
 	if (!list || !*list || !(*list)->next)
@@ -24,8 +24,28 @@ void	reverse_rotate(t_list **list)
 	last = *list;
 	while (last->next != NULL)
 		last = last->next;
-	last->next = (*list);
-	new_head = last;
-	
-	*list = new_head;
+	new_tail = last->previous;
+	new_tail-> next = NULL;
+	last->next = head;
+	head->previous = last;
+	*list = last; 
+	(*list)->previous =  NULL;
+}
+
+void	rra(t_list **list)
+{
+	reverse_rotate(list);
+	printf("%s\n", "rra");
+}
+
+void	rrb(t_list **list)
+{
+	reverse_rotate(list);
+	printf("%s\n", "rrb");
+}
+void	rrr(t_list **list1,  t_list **list2)
+{
+	reverse_rotate(list1);
+	reverse_rotate(list2);
+	printf("%s\n", "rrr");
 }

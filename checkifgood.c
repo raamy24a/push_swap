f07035f03eb040332eb94b11844b10fec4305389
raamydib@gmail.com <radib@fu-r8-p7.s19.be>
2025-05-22 18:05:43 +0200

    hmm segfault a fix mais ou sinon c'est assez bien

--- checkifgood.c
+++ checkifgood.c
@@ -6,34 +6,51 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:03:40 by radib             #+#    #+#             */
-/*   Updated: 2025/05/22 15:55:17 by radib            ###   ########.fr       */
+/*   Updated: 2025/05/22 18:04:11 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	dist(t_list **b, int i)
+int	is_rotate_faster(t_list **b, int i)
 {
 	t_list	*temp;
 	int		sizehalf;
 	int		j;
-	int		trueorfalse;
 
 	sizehalf = i / 2;
 	j = 0;
-	trueorfalse = 0;
 	temp = (*b);
+	if (b && temp->index == i)
+		return (2);
 	while (b && j < sizehalf)
 	{
 		temp = temp->next;
 		j++;
 		if (temp->index == i)
-		{
-			trueorfalse = 1;
-			break ;
-		}
+			return (1);
 	}
-	return (trueorfalse);
+	return (0);
+}
+
+int	dist_rb_pa_rrb(t_list **a, t_list **b, int i)
+{
+	int	rrb_ra_pa;
+
+	rrb_ra_pa = is_rotate_faster(b, i);
+	if (rrb_ra_pa == 0)
+	{
+		rrb(b);
+		return (0);
+	}
+	if (rrb_ra_pa == 1)
+	{
+		rb(b);
+		return (0);
+	}
+	if (rrb_ra_pa == 2)
+		pa(a, b);
+	return (1);
 }
 
 int	swapina(int toswapina, t_list **a)
@@ -86,16 +103,16 @@ void	findorderr(int toswapina, int size, t_list **a, t_list **b)
 			i -= swapina(toswapina, a);
 			toswapina = 0;
 		}
-		if (dist(b, i))
-			rb(b);
-		else
-			rrb(b);
+		i -= dist_rb_pa_rrb(a, b, i);
 	}
 }
 
 void	findorder(int size, t_list **a, t_list **b)
 {
+	// if (size < 250)
 	chunkingloop(size, 5.0, a, b);
+	// else
+		// chunkingloop(size, 11.0, a, b);
 	// if (size < 110)
 	// 	chunkings(size, 0, a, b);
 	// else
